b01c62aa8efc43dbb089ed2a4db9b8763954838a
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-23 17:51:00 -0800

    Adds comment to Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,15 +3,12 @@ function Modal(props) {
     props.onCancel();
   }
 
-  function confirmHandler() {
-    props.onConfirm();
-  }
-  
   return(
     <div className="modal">
       <p>Are you sure?</p>
+      {/* Alternate ways to handle click events. Either calling a function within this file or passing the function from another file using props */}
       <button className="btn btn--alt" onClick={cancelHandler}>Cancel</button>
-      <button className="btn" onClick={confirmHandler}>Confirm</button>
+      <button className="btn" onClick={props.onConfirm}>Confirm</button>
     </div>
   );
 }
